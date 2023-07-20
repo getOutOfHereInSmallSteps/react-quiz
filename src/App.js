6a8b3e75aee956086e844be5fb456c3daef01eea
Alex Bihas <alex.bigas.2004@gmail.com>
2023-07-20 13:32:43 +0300

    Setup useReducer state and questions fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 
-import data from './questions.json';
+import data from './data/questions.json';
+
+import Header from './components/Header';
+import Main from './components/Main';
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: 'loading',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DATA_RECEIVED': {
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    }
+    case 'DATA_FAILED': {
+      return {
+        state,
+        status: 'error',
+      };
+    }
+    case '': {
+      return;
+    }
+
+    default: {
+      throw new Error('Action unknown');
+    }
+  }
+};
 
 const App = () => {
-  return <div></div>;
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch('http://localhost:9000/questions')
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'DATA_RECEIVED', payload: data }))
+      .catch(err => dispatch({ type: 'DATA_FAILED' }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>{status}</Main>
+    </div>
+  );
 };
 
 export default App;
--- src/components/DateCounter.js
+++ src/components/DateCounter.js
@@ -1,34 +1,121 @@
-import { useState } from "react";
+import { useReducer, useState } from 'react';
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  // const [count, setCount] = useState(0);
+  // const [step , setStep] = useState(1);
+
+  const initialState = {
+    count: 0,
+    step: 1,
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'DEC': {
+        return {
+          ...state,
+          count: state.count - state.step,
+        };
+      }
+      case 'INC': {
+        return {
+          ...state,
+          count: state.count + state.step,
+        };
+      }
+      case 'SET_COUNT': {
+        return {
+          ...state,
+          count: action.payload,
+        };
+      }
+      case 'SET_STEP': {
+        return {
+          ...state,
+          step: action.payload,
+        };
+      }
+      case 'RESET': {
+        return initialState;
+      }
+      default: {
+        return state;
+      }
+    }
+
+    // if (action.type === 'DEC') {
+    //   // return state - 1;
+    //   return {
+    //     ...state,
+    //     count: state.count - state.step,
+    //   };
+    // }
+
+    // if (action.type === 'INC') {
+    //   // return state + 1;
+    //   return {
+    //     ...state,
+    //     count: state.count + state.step,
+    //   };
+    // }
+
+    // if (action.type === 'SET_COUNT') {
+    //   // return action.payload;
+    //   return {
+    //     ...state,
+    //     count: action.payload,
+    //   };
+    // }
+
+    // if (action.type === 'SET_STEP') {
+    //   return {
+    //     ...state,
+    //     step: action.payload,
+    //   };
+    // }
+
+    // if (action.type === 'RESET') {
+    //   // return 0;
+    //   return {
+    //     count: 0,
+    //     step: 1,
+    //   };
+    // }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
-  const date = new Date("june 21 2027");
+  const date = new Date('june 21 2027');
   date.setDate(date.getDate() + count);
 
   const dec = function () {
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount(count => count - step);
+    dispatch({ type: 'DEC' });
   };
 
   const inc = function () {
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount(count => count + step);
+    dispatch({ type: 'INC' });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    // setCount(Number(e.target.value));
+    dispatch({ type: 'SET_COUNT', payload: +e.target.value });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
+    dispatch({ type: 'SET_STEP', payload: +e.target.value });
   };
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    // setCount(0);
+    // setStep(1);
+    dispatch({ type: 'RESET' });
   };
 
   return (
